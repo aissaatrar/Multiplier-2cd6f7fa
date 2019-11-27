4d5df9ff6002ead645b1c24b5860d8d6bb7fc5dc
ilias atrar <23261@ma-web.nl>
2019-11-27 10:22:25 +0100

    Uitwerking van de exercise

---
+++ multiplier.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$getal;
+$getal1;
+$resultaat;
+echo 'Eerste getal? ';
+$getal = readline();
+echo 'Tweede getal? ';
+$getal1 = readline();
+$resultaat = $getal * $getal1;
+echo 'Uw resultaat is: ' . $resultaat;
+
+?>
